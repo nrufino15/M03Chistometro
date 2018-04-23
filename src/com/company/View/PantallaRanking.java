1d9a186b4b0b3e52150916b8b2ec9a0c3ecc5663
csanchez <csanchez@elpuig.xeill.net>
2018-04-23 20:32:11 +0200

    fefe

---
+++ src/com/company/View/PantallaRanking.java
@@ -0,0 +1,13 @@
+package com.company.View;
+
+public class PantallaRanking {
+
+    System.out.println("Ranking de chistes")
+    System.out.println("Pos     Valoracion  Chiste")
+    System.out.println("#1"     valoracion  chiste )
+    System.out.println("#2"     valoracion  chiste )
+    System.out.println("#3"     valoracion  chiste )
+
+
+
+}
